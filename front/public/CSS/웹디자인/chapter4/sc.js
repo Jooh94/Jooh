45b244a0b2e4f8851f64ae5caf92cf054985b1b6
504 <504@DESKTOP-36TM4MI>
2022-08-30 18:06:49 +0900

    개인 저장

--- front/public/CSS/웹디자인/chapter4/sc.js
+++ front/public/CSS/웹디자인/chapter4/sc.js
@@ -16,14 +16,14 @@ let pieData = {
 }
 let pieOptions ={
 	width: '100%',
-	height:  '400px'
+	height:  '440px'
 }
 
 new Chartist.Pie('.pie_chart',pieData ,pieOptions)
 
 /* 막대차트 */
 let barData = {
-	lablles : ['2018년','2019년','2020년'],
+	lables : ['2018년','2019년','2020년'],
 	series : [[10,16,29]]
 }
 let barOptions = {
@@ -31,13 +31,13 @@ let barOptions = {
 	//y축
 	axisY : {  //* let 객체명 = {키 :{ } }
 		offset :60 ,								//1. 
-		scaleMinSpa : 50,							//2. 선 간격[px]
+		scaleMinSpace : 50,							//2. 선 간격[px]
 		labelInterpolationFnc : function(value){	//3. 3.Y축 눈금표시
-			return value + ' 명'
+			return value + ' 원'
 		}
 	},
 	width : '100%',
-	height : '400px;'
+	height : '440px'
 }
 
 new Chartist.Bar('.bar_chart',barData,barOptions)
@@ -46,7 +46,7 @@ new Chartist.Bar('.bar_chart',barData,barOptions)
 	
 	
 	let barData2 = {
-	lablles : [],
+	lables : [],
 	series : [[],[]]
 }
 let barOptions2 = {
@@ -60,14 +60,85 @@ let barOptions2 = {
 		}
 	},
 	width : '100%',
-	height : '400px;'
+	height : '440px;'
 }
 function 차트그리기 (){
 	const name = document.getElementById('name').value;
 	const money = document.getElementById('money').value;
 	
-	barData2.lablles.push(name)
+	barData2.lables.push(name)
 	barData2.series[0].push(money)
 
 new Chartist.Bar('.bar_chart2',barData2,barOptions2)
 }
+
+
+
+
+
+/*
+	1.전송버튼을 눌렀을때(클릭) 이벤트 함수[정의]실행
+	
+	2.HTML 에서 입력한 데이터 js 가져와서 변수/객체 저장 -> 확인
+		2-1 : 문서객체 가져오기
+		document.querySelector( ) : 1개 요소 호출
+		document.querySelectorAll ( ) :여러개 호출
+		document.getElementByID( ) :1개 요소
+		2-2 문서개체내 내용을 호출 [ value 속성]
+		2-3 모든 내용을 객체에 저장
+	3. 배열내 존재하는 모든 객체들을 출력
+
+
+*/
+/*-------------------------------*/
+let 지원자리스트 = [ ] 
+
+//1.
+function 지원자등록(){
+	console.log('지원자등록 함수 실행') //1번 테스트
+	let 입력리스트=document.querySelectorAll('input')
+	let 목록상자=document.querySelector('select')
+	let 긴글상자=document.querySelector('textarea')
+	//2-1 테스트
+	console.log(입력리스트)
+	console.log(목록상자)
+	console.log(긴글상자)
+	let 지원자 ={
+		성명: 입력리스트[0].value, //2-2 
+		영문명:입력리스트[1].value,
+		메일주소:입력리스트[2].value,
+		포트폴리오:입력리스트[3].value,
+		희망근무지:입력리스트[4].value,
+		현재상태:목록상자.value,
+		자기소개:긴글상자,value
+	}
+	//2-3번테스트
+	console.log(지원자)
+	//2-4
+	지원자리스트.push(지원자)
+	console.log(지원자리스트)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

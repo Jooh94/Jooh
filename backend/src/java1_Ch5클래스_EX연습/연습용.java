16e3dfc4cc27426f2a543258e4700bc76088dd86
504 <504@DESKTOP-36TM4MI>
2022-09-19 20:51:08 +0900

    9 19

---
+++ backend/src/java1_Ch5클래스_EX연습/연습용.java
@@ -0,0 +1,19 @@
+package java1_Ch5클래스_EX연습;
+
+public class 연습용 {
+public static void main(String[] args) {
+	
+	boolean 불1 = true;
+	byte 바이트1 = 100; 
+	char 문자열1 = 'A'; 
+	short 쇼트1 = 30000; 
+	int 인트1 = 2000000000; 
+	long 롱1 = 2000000000L; 
+	float 풀릇1 = 3.12345678F;
+	double 더블1 = 3.1234567891; 
+	
+
+	
+	
+}
+}
